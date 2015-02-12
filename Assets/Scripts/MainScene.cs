ed8e07a76619b0c16a7a42d2db2a8cf6c82e6075
gomse <sehwan@gomse.net>
2015-02-12 17:27:39 +0900

    메인씬에 스크립트 연결.

--- Assets/Scripts/MainScene.cs
+++ Assets/Scripts/MainScene.cs
@@ -3,13 +3,26 @@ using System.Collections;
 using System.Collections.Generic;
 
 
-public class MainScene : MonoBehaviour {
-
-	public void Awake() {
+public class MainScene : MonoBehaviour, IInputEventListener
+{
+	public void Awake()
+	{
+		InputManager.Instance.Init();
 	}
 
 	// Use this for initialization
-	public void Start() {
+	public void Start()
+	{
+		InputManager.Instance.AddEventListener(this);
 	}
 
+	public void OnDestory()
+	{
+		InputManager.Instance.RemoveEventListener(this);
+	}
+
+	public void OnTouchEvent(eTouchPhase phase, int fingerId, Vector2 pos, Vector2 delta)
+	{
+		Debug.Log("OnTouchEvent: " + phase.ToString());
+	}
 }
